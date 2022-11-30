876b9bb61434bd632ef25fe1d39a3550209c073c
Martin Kavalar <martin@nextjournal.com>
2022-11-30 09:13:47 +0100

    Day 1

--- src/advent_of_clerk/day_01.clj
+++ src/advent_of_clerk/day_01.clj
@@ -1,3 +1,16 @@
 ;; # 🎄 Advent of Clerk: Day 1
 (ns advent-of-clerk.day-01
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [nextjournal.clerk :as clerk]))
+
+(def input
+  (slurp (io/resource "day_1.txt")))
+
+(->> (str/split-lines input)
+     (partition-by str/blank?)
+     (filterv #(not= % [""]))
+     (mapv (fn [xs] (mapv #(Integer/parseInt %) xs)))
+     (map #(reduce + %))
+     (apply max))
+
