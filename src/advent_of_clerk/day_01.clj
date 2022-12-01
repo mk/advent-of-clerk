57647270bd0cfdf76a82842cc8f2535f2d7ba289
Martin Kavalar <martin@nextjournal.com>
2022-12-01 13:26:15 +0100

    Add day 1 part 2

--- src/advent_of_clerk/day_01.clj
+++ src/advent_of_clerk/day_01.clj
@@ -7,10 +7,15 @@
 (def input
   (slurp (io/resource "day_1.txt")))
 
-(->> (str/split-lines input)
-     (partition-by str/blank?)
-     (filterv #(not= % [""]))
-     (mapv (fn [xs] (mapv #(Integer/parseInt %) xs)))
-     (map #(reduce + %))
-     (apply max))
+;; ## Part 1
+(def calories-sums
+  (->> (str/split-lines input)
+       (partition-by str/blank?)
+       (filterv #(not= % [""]))
+       (mapv (fn [xs] (mapv #(Integer/parseInt %) xs)))
+       (map #(reduce + %))))
 
+(reduce max calories-sums)
+
+;; ## Part 2
+(reduce + (take 3 (reverse (sort calories-sums))))
